e3e913aa27efbd034bfe4d49b422a3b0fa71b41c
shriraj <jagtapshriraj6@gmail.com>
2025-11-18 15:59:00 +0530

    Spring Boot Setup

---
+++ src/test/java/com/workflow/demo/WorkflowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workflow.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkflowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
